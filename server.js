c0fe484c758f9a2f6b83804167eee27dfe84690c
anuj <anujkumar666768@gmail.com>
2024-03-04 10:10:03 +0530

    Committing changes

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const { exec } = require('child_process');
 const express = require('express')
 const app = express();
 app.use(express.json())
-function pushToGitHub(commitMessage) {
-  const gitCommand = `git add seconde.txt && git commit -m "${commitMessage}" && git push origin master`;
-
-  exec(gitCommand, (error, stdout, stderr) => {
+const pushToGitHub = async (commitMessage) => {
+  const gitCommand = `git add . && git commit -m "${commitMessage}" && git push origin branch`;
+  const newDirectory = 'D:/node scheduler/newf';
+  // process.chdir(newDirectory);
+  // console.log(process.cwd())
+  await exec(gitCommand, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error executing Git command: ${error.message}`);
       return;
@@ -20,11 +22,28 @@ function pushToGitHub(commitMessage) {
   });
 }
 
-app.get("/", (req, res) => {
+const checkbranch=async(newbranch)=>{
+  const gitCommand=`git branch ${newbranch}`;
+ await exec(gitCommand,(err)=>{
+    if(err){
+      console.log(err)
+    }
+  })
+}
+
+
+
+app.get("/push", (req, res) => {
   const commitMessage = "Committing changes";
   pushToGitHub(commitMessage);
   res.send("done!")
 })
+// app.get("/check",async(req,res)=>{
+//   const commitMessage='git branch add'
+//   const newbranch="branch"
+//  await checkbranch(newbranch);
+//  console.log("checking branch")
+// })
 app.listen(3000, () => {
   console.log("the server is runnning on port 3000")
 })
